99601fb7d884adc10ad4141b66f1cd1e74d0c32a
David Falla Urtiaga <falla.urdav24@triana.salesianos.edu>
2025-01-23 13:44:55 +0100

    proyecto creado

---
+++ ejercicio_herencia/src/main/java/com/salesianostriana/dam/ejercicio_herencia/EjercicioHerenciaApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.ejercicio_herencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioHerenciaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjercicioHerenciaApplication.class, args);
+    }
+
+}
---
+++ ejercicio_herencia/src/test/java/com/salesianostriana/dam/ejercicio_herencia/EjercicioHerenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.ejercicio_herencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioHerenciaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
